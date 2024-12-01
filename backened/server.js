f601dbc8246a85841157a227bb6ce6a2782b4cb8
Abhay7286 <abhaymishra7286@gmail.com>
2024-12-01 20:42:15 +0530

    1st commit
    authentication endpoints

---
+++ backened/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import { connectToDb } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cookieParser());
+app.use(express.json()); //allows you to parse the body of the request
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT,() => {
+  console.log(`server is running on ${PORT}`);
+  connectToDb();
+})
